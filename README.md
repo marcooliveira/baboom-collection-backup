f562c59c4a7e2ad37f9f6ca14a2ba3ccb5a12d99
Marco Oliveira <marcooliveira@Marcos-MBP.lan>
2016-06-17 22:35:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# BABOOM Collection Backup
+
+This tool allows you to quickly backup all your collection into your computer.
+
+## Installation
+
+You will need [Node.js](https://nodejs.org) for this.
+
+Just clone this repository and run `npm install`.
+
+## Usage
+
+Just run `npm start` inside the repository folder, and the script will ask for
+your login credentials. The result of the backup will be inside a `songs/`
+folder.
---
+++ index.js
@@ -0,0 +1,123 @@
+const promptly = require('promptly');
+const got      = require('got');
+const fs       = require('fs');
+const async    = require('async');
+const mkdirp   = require('mkdirp');
+const path     = require('path');
+const planify  = require('planify');
+const api      = new (require('./lib/Api'))();
+
+// <CONFIG>
+const limitPerPage        = 100;
+const downloadConcurrency = 5;
+const format              = 'mp3_320k'; // available options are: flac, mp3_320k, mp3_192k, ogg_vorbis_q9, ogg_vorbis_q5, ogg_vorbis_q2
+// </CONFIG>
+
+planify({ reporter: 'blocks' })
+
+.phase('Generate user credentials', phase => {
+  phase.step('Get user details', (data, done) => {
+    promptly.prompt('Email: ', (err, email) => {
+      if (err) {
+        return done(err);
+      }
+
+      data.email = email;
+
+      promptly.password('Password: ', (err, password) => {
+        if (err) {
+          return done(err);
+        }
+
+        data.password = password;
+
+        return done();
+      })
+    })
+  })
+  .step('Login', data => {
+    return api.login(data.email, data.password)
+    .then(user => {
+      console.log('Logged in as:');
+      console.log(JSON.stringify(user, null, 2));
+    });
+  })
+})
+
+.phase('Gather collection list', phase => {
+  phase.step('Fetch list of songs', data => {
+    return api.songs(0, limitPerPage)
+    .then(res => {
+      data.requests = [];
+
+      // initialise songs list with first page that was fetched
+      data.songs = res.items;
+
+      let offset = limitPerPage;
+      data.total = res.meta.total;
+
+      // prepare all requests to be made
+      while (offset <= data.total) {
+        let req = api.songs(offset, limitPerPage)
+                  .then(res => data.songs = data.songs.concat(res.items));
+
+        data.requests.push(req);
+
+        offset += limitPerPage;
+      }
+
+      // wait for all pages to be fetched
+      return Promise.all(data.requests)
+        .then(() => console.log(`${data.songs.length} songs found`));
+    });
+  })
+
+  .step('Store songs metadata', (data, done) => {
+    mkdirp('songs', err => {
+      if (err) {
+        throw err;
+      }
+
+      fs.writeFileSync(path.join('songs', 'songs.json'), JSON.stringify(data.songs, null, 2));
+
+      return done();
+    });
+  });
+})
+
+.step(`Download songs`, data => {
+  return new Promise((resolve, reject) => {
+    console.log('total songs', data.songs.length);
+    async.eachLimit(data.songs, downloadConcurrency, (song, callback) => {
+      let folder        = path.join('songs', song.display_artist, `${song.album.display_artist} - ${song.album.title}`);
+      let songFormat    = song.stream.audio.tags.indexOf(format) >= 0 ? format : song.stream.audio.tags[0]; // if the prefered format is available, use, else use the available one
+      let fileExtension = songFormat.split('_')[0];
+      let filePath      = path.join(folder, `${song.display_artist} - ${song.title}.${fileExtension}`);
+
+      mkdirp(folder, (err) => {
+        if (err) {
+          return callback(err);
+        }
+
+        // get file
+        console.log(`Downloading ${filePath}`);
+        api.songStream(song.stream.audio.url, songFormat)
+          .on('error', callback)
+        // and write into disk
+        .pipe(fs.createWriteStream(filePath))
+          .on('error', callback)
+          .on('finish', callback);
+      });
+    }, (err) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve();
+    });
+  });
+})
+.run({})
+.catch(err => console.log(err))
+// Run returns a promise but callback style is also supported
+// .then(() => process.exit(), (err) => process.exit(1));
